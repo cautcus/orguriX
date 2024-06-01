2df0a817af81d0956e870dd78256c54dc6b03522
Ayush Baral <ayushbaral010@gmail.com>
2024-06-01 16:08:17 +0545

    0.0.3(Donation)

--- src/components/donate.jsx
+++ src/components/donate.jsx
@@ -4,7 +4,7 @@ import { Alert } from "flowbite-react";
 export default function Donate() {
   return (
     <Alert color='green' className='flex items-center'>
-    <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    <a className="font-medium" href="/donation">Info alert!</a> Change a few things up and try submitting again.
   </Alert>
   );
 }
